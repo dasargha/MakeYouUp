226ea5cbd1b2ae156ec8d076f4bf9d30b1e4f084
Argha <arghadas06@gmail.com>
2017-11-06 10:21:57 +0530

    6 nov Argha

--- app/src/main/java/com/makeuup/app/makeyouup/activity/DashActivity.java
+++ app/src/main/java/com/makeuup/app/makeyouup/activity/DashActivity.java
@@ -1,4 +1,4 @@
-package com.makeuup.app.makeyouup;
+package com.makeuup.app.makeyouup.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,7 +9,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.makeuup.app.makeyouup.R;
+
+public class DashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
